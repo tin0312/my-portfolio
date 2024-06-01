422e7ff6f05f0bf94e01e7bedea5d244e58dc3aa
unknown <justinhoang0312@gmail.com>
2024-06-01 11:37:01 -0400

    Initialize basic page layout and MUI app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Box from "@mui/material/Box";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import "./App.css";
+
 export default function App() {
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Box
+        className="site-wrapper"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          minHeight: "100vh",
+        }}
+      >
+        <Box className="site-header">
+          <Header />
+        </Box>
+        <Box className="site-main">
+          <About />
+          <Projects />
+          <Contact />
+        </Box>
+        <Box className="site-footer">
+          <Footer />
+        </Box>
+      </Box>
     </>
   );
 }
