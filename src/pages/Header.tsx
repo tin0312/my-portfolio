ab219af53d8f545b037eb624ed14e606a6c106bf
Hoang Nhat Truong <105316547+tin0312@users.noreply.github.com>
2024-06-05 17:43:28 -0400

    Update Header.tsx

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,262 +1,203 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
+import { useEffect, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import WavingHandIcon from "@mui/icons-material/WavingHand";
-import CloseIcon from "@mui/icons-material/Close";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
-import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import { useTheme } from "@mui/material/styles";
-import ModeToggle from "../component/ModeToggle";
+import Box from "@mui/material/Box";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import "./App.css";
 
-const drawerWidth = 240;
-type NavItem = "home" | "about" | "project" | "contact";
+const themeLight = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
-const navItems: NavItem[] = ["home", "about", "project", "contact"];
+const themeDark = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
-interface Props {
-  window?: () => Window;
-  toggleTheme: () => void;
-}
+export default function App() {
+  const [theme, setTheme] = useState(themeLight);
+  const [loading, setLoading] = useState(true);
 
-const Header: React.FC<Props> = (props) => {
-  const { window, toggleTheme } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [activeItem, setActiveItem] = React.useState<NavItem>("home");
-  const theme = useTheme();
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === themeLight ? themeDark : themeLight));
   };
 
-  const getIcon = (item: NavItem) => {
-    switch (item) {
-      case "home":
-        return (
-          <HomeOutlinedIcon
-            sx={{
-              fontSize: { xs: "2.5rem", sm: "1.5rem" },
-              color: activeItem === item ? "#D07225" : "#FFF",
-            }}
-          />
-        );
-      case "about":
-        return (
-          <PersonOutlineIcon
-            sx={{
-              fontSize: { xs: "2.5rem", sm: "1.5rem" },
-              color: activeItem === item ? "#D07225" : "#FFF",
-            }}
-          />
-        );
-      case "project":
-        return (
-          <WorkOutlineIcon
-            sx={{
-              fontSize: { xs: "2.5rem", sm: "1.5rem" },
-              color: activeItem === item ? "#D07225" : "#FFF",
-            }}
-          />
-        );
-      case "contact":
-        return (
-          <AlternateEmailIcon
-            sx={{
-              fontSize: { xs: "2.5rem", sm: "1.5rem" },
-              color: activeItem === item ? "#D07225" : "#FFF",
-            }}
-          />
-        );
-      default:
-        return null;
-    }
-  };
-
-  const drawer = (
-    <Box
-      className="site-wrapper"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        textAlign: "center",
-        backgroundImage:
-          theme.palette.mode === "light"
-            ? "linear-gradient(rgb(37, 131, 208, 0.9), rgba(14, 63, 104, 0.25))"
-            : "none",
-            backgroundColor: theme.palette.mode === 'dark' ? '#121212' : '#fff', // Customize based on the mode
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          paddingX: 2,
-        }}
-      >
-        <Box sx={{ display: "flex", alignItems: "center", height: "6.25rem" }}>
-          <Typography sx={{ fontSize: "1.7rem", fontWeight: "bold" }}>
-            h i
-          </Typography>
-          <WavingHandIcon
-            className="waving-hand"
-            sx={{ color: "gold", marginLeft: "0.7rem", fontSize: "1.7rem" }}
-          />
-        </Box>
-        <IconButton onClick={handleDrawerToggle} sx={{ color: "#FFF" }}>
-          <CloseIcon sx={{fontSize: "2.8rem"}}/>
-        </IconButton>
-      </Box>
-      <Divider />
-      <List
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          height: "90vh",
-          justifyContent: "space-around",
-        }}
-      >
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton
-              onClick={() => setActiveItem(item)}
-              href={`#${item}`}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                textAlign: "center",
-                "&:hover": {
-                  transform: " translateY(-4px), translateX(-2px)",
-                  boxShadow: "0px 0px 20px 5px  #2583D0",
-                },
-              }}
-            >
-              {getIcon(item)}
-              <ListItemText
-                primary={item}
-                primaryTypographyProps={{
-                  fontSize: "1.2rem",
-                  fontWeight: "bold",
-                  color: "#FFF",
-                  letterSpacing: "0.1rem",
-                }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000); // Simulating a loading time
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar
-        component="nav"
-        sx={{
-          background: "transparent",
-          height: "6.25rem",
-          display: "flex",
-          justifyContent: "center",
-           boxShadow: theme.palette.mode === 'light' 
-                ? ''
-                : '0px 4px 6px rgba(255, 255, 255, 0.1)', 
-        }}
-      >
-        <Toolbar
+      {loading ? (
+        <Box
           sx={{
             display: "flex",
-            justifyContent: "space-between",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100vw",
+            height: "100vh",
+            backgroundImage: theme.palette.mode === "light"
+              ? "linear-gradient(rgb(37, 131, 208, 0.9), rgba(14, 63, 104, 0.25))"
+              : "none",
           }}
         >
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+          <Box
+            sx={{
+              width: '250px',
+              height: '250px',
+              padding: '15px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'relative',
+            }}
           >
-            <MenuIcon sx={{fontSize: "2.8rem"}}/>
-          </IconButton>
-          <Typography
-            variant="h4"
-            component="div"
-            sx={{ letterSpacing: "0.5rem" }}
-          >
-            truong
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "flex" }, gap: "1rem" }}>
-            {navItems.map((item) => (
-              <Button
-                key={item}
-                onClick={() => setActiveItem(item)}
+            <Box
+              sx={{
+                width: '250px',
+                position: 'absolute',
+                zIndex: 11,
+                animation: 'clouds 8s infinite ease-in-out',
+                display: 'flex',
+                paddingTop: '45px',
+                marginLeft: '25px',
+              }}
+            >
+              <Box
                 sx={{
-                  display: "flex",
-                  gap: "0.5rem",
-                  color: "#fff",
-                  textTransform: "lowercase",
-
-                  letterSpacing: "4px",
-                  fontSize: "1.2rem",
-                  "&:hover": {
-                    transform: " translateY(-4px), translateX(-2px)",
-                    boxShadow: "2px 5px 0 0 #2583D0",
-                  },
+                  width: '65px',
+                  height: '65px',
+                  borderRadius: '50% 50% 0 50%',
+                  backgroundColor: '#4c9beb',
+                  display: 'inline-block',
                 }}
-                href={`#${item}`}
-                className={activeItem === item ? "active" : ""}
-              >
-                {getIcon(item)}
-                {item}
-              </Button>
-            ))}
+              ></Box>
+              <Box
+                sx={{
+                  width: '45px',
+                  height: '45px',
+                  borderRadius: '50% 50% 50% 0',
+                  backgroundColor: '#4c9beb',
+                  display: 'inline-block',
+                  marginLeft: '-25px',
+                }}
+              ></Box>
+            </Box>
+            <Box
+              sx={{
+                width: '120px',
+                height: '120px',
+                background: 'linear-gradient(to right, #fcbb04, #fffc00)',
+                borderRadius: '60px',
+                position: 'absolute',
+              }}
+              className="sunshine"
+            ></Box>
+            <Box
+              sx={{
+                width: '120px',
+                height: '120px',
+                background: 'linear-gradient(to right, #fcbb04, #fffc00)',
+                borderRadius: '60px',
+                position: 'absolute',
+                animation: 'sunshines 2s infinite',
+              }}
+            ></Box>
+            <Box
+              sx={{
+                width: '250px',
+                position: 'absolute',
+                zIndex: 12,
+                animation: 'clouds 12s infinite ease-in-out',
+                display: 'flex',
+                marginTop: '-30px',
+                marginLeft: '150px',
+              }}
+            >
+              <Box
+                sx={{
+                  width: '30px',
+                  height: '30px',
+                  borderRadius: '50% 50% 0 50%',
+                  backgroundColor: '#4c9beb',
+                  display: 'inline-block',
+                }}
+              ></Box>
+              <Box
+                sx={{
+                  width: '50px',
+                  height: '50px',
+                  borderRadius: '50% 50% 50% 0',
+                  backgroundColor: '#4c9beb',
+                  display: 'inline-block',
+                  marginLeft: '-20px',
+                }}
+              ></Box>
+            </Box>
           </Box>
-          <IconButton
-            color="inherit"
-            aria-label="toggle light dark mode"
-            onClick={toggleTheme}
-          >
-            <ModeToggle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
+        </Box>
+      ) : (
+        <Box
+          className="site-wrapper"
           sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            minHeight: "100vh",
+            backgroundImage:
+              theme.palette.mode === "light"
+                ? "linear-gradient(rgb(37, 131, 208, 0.9), rgba(14, 63, 104, 0.25))"
+                : "none",
+            backgroundColor: theme.palette.background.default,
+            transition:
+              "background-color 0.5s ease-in-out, background-image 0.5s ease-in-out",
           }}
         >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
+          <Box className="site-header">
+            <Header toggleTheme={toggleTheme} />
+          </Box>
+          <Box className="site-main">
+            <About />
+            <Projects />
+            <Contact />
+          </Box>
+          <Box className="site-footer">
+            <Footer />
+          </Box>
+        </Box>
+      )}
+      <style>{`
+        @keyframes sunshines {
+          0% {
+            transform: scale(1);
+            opacity: 0.6;
+          }
+          100% {
+            transform: scale(1.4);
+            opacity: 0;
+          }
+        }
+        @keyframes clouds {
+          0% {
+            transform: translateX(15px);
+          }
+          50% {
+            transform: translateX(0px);
+          }
+          100% {
+            transform: translateX(15px);
+          }
+        }
+      `}</style>
+    </ThemeProvider>
   );
-};
-
-export default Header;
+}
