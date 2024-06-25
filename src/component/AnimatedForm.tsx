f02c63413a38c65af98bb4dc1af037628fca083b
unknown <justinhoang0312@gmail.com>
2024-06-24 20:15:05 -0400

    Adjust contact messagage font size on mobile

--- src/component/AnimatedForm.tsx
+++ src/component/AnimatedForm.tsx
@@ -1,48 +1,52 @@
-import { useForm, Controller } from 'react-hook-form';
-import { TextField, Button, Box, Typography } from '@mui/material';
-import { styled } from '@mui/system';
-import { useTheme } from '@mui/material/styles';
+import { useForm, Controller } from "react-hook-form";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { useTheme } from "@mui/material/styles";
 
 const AnimatedContainer = styled(Box)(({ theme }) => ({
-  width: '600px',
+  width: "600px",
   "@media (max-width: 600px)": {
     width: "100%",
-    height: "400px"
+    height: "400px",
   },
-  background: theme.palette.mode === 'light'
-    ? 'linear-gradient(rgba(240, 240, 240, 0.1), rgba(240, 240, 240, 0.1)) padding-box, linear-gradient(145deg, transparent 35%, #e0761f, #1F89E0) border-box'
-    : 'linear-gradient(#212121, #212121) padding-box, linear-gradient(145deg, transparent 35%, #e0761f, #1F89E0) border-box',
-  border: '2px solid transparent',
-  padding: '32px 24px',
-  fontSize: '14px',
-  fontFamily: 'inherit',
-  color: theme.palette.mode === 'light' ? '#000' : '#fff',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '20px',
-  boxSizing: 'border-box',
-  borderRadius: '16px',
-  backgroundSize: '200% 100%',
-  animation: 'gradient 5s ease infinite',
-  '@keyframes gradient': {
-    '0%': { backgroundPosition: '0% 50%' },
-    '50%': { backgroundPosition: '100% 50%' },
-    '100%': { backgroundPosition: '0% 50%' },
-  }
+  background:
+    theme.palette.mode === "light"
+      ? "linear-gradient(rgba(240, 240, 240, 0.1), rgba(240, 240, 240, 0.1)) padding-box, linear-gradient(145deg, transparent 35%, #e0761f, #1F89E0) border-box"
+      : "linear-gradient(#212121, #212121) padding-box, linear-gradient(145deg, transparent 35%, #e0761f, #1F89E0) border-box",
+  border: "2px solid transparent",
+  padding: "32px 24px",
+  fontSize: "14px",
+  fontFamily: "inherit",
+  color: theme.palette.mode === "light" ? "#000" : "#fff",
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+  boxSizing: "border-box",
+  borderRadius: "16px",
+  backgroundSize: "200% 100%",
+  animation: "gradient 5s ease infinite",
+  "@keyframes gradient": {
+    "0%": { backgroundPosition: "0% 50%" },
+    "50%": { backgroundPosition: "100% 50%" },
+    "100%": { backgroundPosition: "0% 50%" },
+  },
 }));
 
 const AnimatedForm = () => {
-  const { control, formState: { errors } } = useForm();
+  const {
+    control,
+    formState: { errors },
+  } = useForm();
   const theme = useTheme();
 
-//   const onSubmit = (data) => {
-//     console.log(data);
-//   };
+  //   const onSubmit = (data) => {
+  //     console.log(data);
+  //   };
 
   return (
     <AnimatedContainer
       component="form"
-    //   onSubmit={handleSubmit(onSubmit)}
+      //   onSubmit={handleSubmit(onSubmit)}
     >
       <Typography variant="h6" component="h1" gutterBottom>
         Contact me
@@ -51,7 +55,7 @@ const AnimatedForm = () => {
         name="email"
         control={control}
         defaultValue=""
-        rules={{ required: 'Email is required' }}
+        rules={{ required: "Email is required" }}
         render={({ field }) => (
           <TextField
             {...field}
@@ -63,25 +67,33 @@ const AnimatedForm = () => {
             required
             InputProps={{
               style: {
-                color: theme.palette.mode === 'light' ? '#000' : '#fff',
-                backgroundColor: 'transparent',
-                borderRadius: '8px',
-                borderColor: theme.palette.mode === 'light' ? '#cccccc' : '#414141'
-              }
+                color: theme.palette.mode === "light" ? "#000" : "#fff",
+                backgroundColor: "transparent",
+                borderRadius: "8px",
+                borderColor:
+                  theme.palette.mode === "light" ? "#cccccc" : "#414141",
+              },
             }}
-            InputLabelProps={{ style: { color: theme.palette.mode === 'light' ? '#717171' : '#cccccc' } }}
+            InputLabelProps={{
+              style: {
+                color: theme.palette.mode === "light" ? "#717171" : "#cccccc",
+              },
+            }}
             sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#cccccc' : '#414141'
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#cccccc" : "#414141",
                 },
-                '&:hover fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#000' : '#e81cff'
+                "&:hover fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#000" : "#e81cff",
                 },
-                '&.Mui-focused fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#000' : '#e81cff'
-                }
-              }
+                "&.Mui-focused fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#000" : "#e81cff",
+                },
+              },
             }}
           />
         )}
@@ -90,7 +102,7 @@ const AnimatedForm = () => {
         name="textarea"
         control={control}
         defaultValue=""
-        rules={{ required: 'This field is required' }}
+        rules={{ required: "This field is required" }}
         render={({ field }) => (
           <TextField
             {...field}
@@ -104,25 +116,33 @@ const AnimatedForm = () => {
             required
             InputProps={{
               style: {
-                color: theme.palette.mode === 'light' ? '#000' : '#fff',
-                backgroundColor: 'transparent',
-                borderRadius: '8px',
-                borderColor: theme.palette.mode === 'light' ? '#cccccc' : '#414141'
-              }
+                color: theme.palette.mode === "light" ? "#000" : "#fff",
+                backgroundColor: "transparent",
+                borderRadius: "8px",
+                borderColor:
+                  theme.palette.mode === "light" ? "#cccccc" : "#414141",
+              },
             }}
-            InputLabelProps={{ style: { color: theme.palette.mode === 'light' ? '#717171' : '#cccccc' } }}
+            InputLabelProps={{
+              style: {
+                color: theme.palette.mode === "light" ? "#717171" : "#cccccc",
+              },
+            }}
             sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#cccccc' : '#414141'
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#cccccc" : "#414141",
                 },
-                '&:hover fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#000' : '#e81cff'
+                "&:hover fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#000" : "#e81cff",
                 },
-                '&.Mui-focused fieldset': {
-                  borderColor: theme.palette.mode === 'light' ? '#000' : '#e81cff'
-                }
-              }
+                "&.Mui-focused fieldset": {
+                  borderColor:
+                    theme.palette.mode === "light" ? "#000" : "#e81cff",
+                },
+              },
             }}
           />
         )}
@@ -132,26 +152,32 @@ const AnimatedForm = () => {
         variant="contained"
         color="primary"
         sx={{
-          alignSelf: 'flex-start',
-          fontFamily: 'inherit',
-          color: theme.palette.mode === 'light' ? '#000' : '#717171',
+          alignSelf: "flex-start",
+          fontFamily: "inherit",
+          color: theme.palette.mode === "light" ? "#000" : "#717171",
           fontWeight: 600,
-          background: theme.palette.mode === 'light' ? 'rgba(240, 240, 240, 0.8)' : '#313131',
-          border: `1px solid ${theme.palette.mode === 'light' ? '#cccccc' : '#414141'}`,
-          padding: '12px 16px',
-          fontSize: 'inherit',
-          gap: '8px',
-          marginTop: '8px',
-          cursor: 'pointer',
-          borderRadius: '6px',
-          '&:hover': {
-            backgroundColor: theme.palette.mode === 'light' ? '#e0e0e0' : '#fff',
-            borderColor: theme.palette.mode === 'light' ? '#000' : '#fff',
-            color: theme.palette.mode === 'light' ? '#000' : '#000'
+          background:
+            theme.palette.mode === "light"
+              ? "rgba(240, 240, 240, 0.8)"
+              : "#313131",
+          border: `1px solid ${
+            theme.palette.mode === "light" ? "#cccccc" : "#414141"
+          }`,
+          padding: "12px 16px",
+          fontSize: "inherit",
+          gap: "8px",
+          marginTop: "8px",
+          cursor: "pointer",
+          borderRadius: "6px",
+          "&:hover": {
+            backgroundColor:
+              theme.palette.mode === "light" ? "#e0e0e0" : "#fff",
+            borderColor: theme.palette.mode === "light" ? "#000" : "#fff",
+            color: theme.palette.mode === "light" ? "#000" : "#000",
           },
-          '&:active': {
-            transform: 'scale(0.95)'
-          }
+          "&:active": {
+            transform: "scale(0.95)",
+          },
         }}
       >
         Submit
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import AnimatedForm from "../component/AnimatedForm";
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
 import { Fade, Zoom } from "react-awesome-reveal";
 
 export default function Contact() {
@@ -18,8 +18,8 @@ export default function Contact() {
           justifyContent: "center",
           alignItems: "center",
           "@media (max-width: 600px)": {
-            alignItems: "flex-start"
-          }
+            alignItems: "flex-start",
+          },
         }}
       >
         <Box
@@ -27,30 +27,58 @@ export default function Contact() {
             width: "80%",
             display: "flex",
             justifyContent: "space-between",
+            alignItems: "center", //
             "@media (max-width: 600px)": {
               alignItems: "center",
               flexDirection: "column",
-              justifyContent:"space-around",
+              justifyContent: "space-around",
               width: "95%",
-  
             },
           }}
         >
-          <Box sx={{width: "600px",  "@media (max-width: 600px)": {
-            width: "100%",
-            marginBottom: 6
-        },}}>
-            <Typography variant="h2" sx={{ marginTop: 15, marginBottom: 7, textShadow:theme.palette.mode === "light" ? "0px 2px 4px rgba(0, 0, 0, 0.3)" : "0px 4px 6px rgba(255, 255, 255, 0.3)" }}>
-                <Fade cascade damping={0.1}>
-                    Say hi
-                </Fade>
+          <Box
+            sx={{
+              width: "1000px",
+              "@media (max-width: 600px)": {
+                width: "100%",
+                marginBottom: 6,
+              },
+            }}
+          >
+            <Typography
+              variant="h2"
+              sx={{
+                marginTop: 15,
+                marginBottom: 7,
+                textShadow:
+                  theme.palette.mode === "light"
+                    ? "0px 2px 4px rgba(0, 0, 0, 0.3)"
+                    : "0px 4px 6px rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              <Fade cascade damping={0.1}>
+                Say hi
+              </Fade>
             </Typography>
-            <Typography variant="body1">
-             Have a project in mind? Send me a message and let's hear them out!  I'm always excited to collaborate and help you bring your ideas to life.
+            <Typography
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.5rem" },
+                color: theme.palette.text.secondary,
+              }}
+            >
+              Have a project in mind? Send me a message and let's hear them out!
+              I'm always excited to collaborate and help you bring your ideas to
+              life.
             </Typography>
           </Box>
-          <Zoom style={{width: "100%"}}>
-              <AnimatedForm />
+          <Zoom
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <AnimatedForm />
           </Zoom>
         </Box>
       </Box>
